9d786e166ca984de6945d211c53fd6ab01cf7e8a
Chandrashekar Venugopala Chimbili <chandrashekar.cv@outlook.com>
2016-01-21 20:01:37 -0800

    UnionFind cycle detection.
    find if there is a cycle in the graph. If yes return True else return
    False.

---
+++ graphs/unionFind.py
@@ -0,0 +1,47 @@
+__author__ = 'Chandu'
+
+class Edge:
+        src = -1
+        dest = -1
+        def __init__(self,src,dest):
+            self.src = src
+            self.dest = dest
+
+class graph:
+    v = 0
+    e = 0
+    edges = []
+
+    def __init__(self,v,e):
+        self.v = v
+        self.e = e
+
+    def find(self,parent, i):
+        if parent[i] == -1:
+            return i
+        else:
+            return self.find(parent,parent[i])
+
+    def union(self,parent,x,y):
+        par_x = self.find(parent,x)
+        par_y = self.find(parent,y)
+        parent[par_x] = par_y
+
+    def isCycle(self):
+        parent = [-1] * self.v
+
+        for edge in self.edges:
+            par_src = self.find(parent,edge.src)
+            par_dest = self.find(parent,edge.dest)
+            if par_src == par_dest:
+                return True
+
+            parent[par_src] = par_dest
+        return False
+
+if __name__ == "__main__":
+    g = graph(3,3)
+    g.edges.append(Edge(0,1))
+    g.edges.append(Edge(1,2))
+    g.edges.append(Edge(0,2))
+    print(g.isCycle())
